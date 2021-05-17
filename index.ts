7e07a2293f2581792e2fb2343d6972cccac7c59e
Simranjeet Singh <smrnjeet222@gmail.com>
2021-05-17 21:24:03 +0530

    types + multiple pages

--- index.ts
+++ index.ts
@@ -2,8 +2,22 @@ import puppeteer from 'puppeteer';
 import fs from 'fs';
 import path from 'path'
 
+// TYPES
+interface IPagesLink {
+  id: string;
+  link: string;
+}
+
+interface IAirportData {
+  iata: string,
+  icao: string,
+  airport_name: string,
+  location_served: string,
+}
+
+
 // Create a file for given json data
-const createFile = (name: string, data: Object): void => {
+const createFile = (name: string, data: IAirportData[]): void => {
 
   const filePath = path.dirname(name)
   // make output directory if doesn't exist, then add file
@@ -13,14 +27,13 @@ const createFile = (name: string, data: Object): void => {
         if (err) throw err;
         console.log(`${name} created`);
       });
-    }
-    )
+    })
 }
 
 
 // PART 1
 // Receive all links of alpabectically ordered pages i.e. List: A-B-C-.... 
-const getAllLinks = async (): Promise<any[]> => {
+const getAllLinks = async (): Promise<IPagesLink[]> => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://en.wikipedia.org/wiki/Lists_of_airports');
@@ -48,55 +61,61 @@ const getAllLinks = async (): Promise<any[]> => {
 
 // PART 2
 // scraping table provided the page link
-const scrapeList = async (link: string): Promise<any[]> => {
-  const browser = await puppeteer.launch();
+const scrapeList = async (browser: puppeteer.Browser, link: string): Promise<IAirportData[]> => {
   const page = await browser.newPage();
   await page.goto(link);
   await page.waitForSelector('.wikitable');
 
-  const data = await page.$$eval('.wikitable', table => {
+  const tableData: IAirportData[] = await page.$$eval('.wikitable',
+    (table): IAirportData[] => {
 
-    // replace whitespaces, commas and numbers
-    const customString = (str: string | null) => {
-      // for empty row data return empty string
-      if (str === null) return ""
+      // replace whitespaces, commas and numbers
+      const customString = (str: string | null): string => {
+        // for empty row data return empty string
+        if (str === null) return ""
 
-      // regex for finding whitespaces and commas
-      const exp = /\s|,/g;
-      const converted = str.toLocaleLowerCase().replace(exp, "_",);
+        // regex for finding whitespaces and commas 
 
-      // replacing number digits w/ 'dapi'
-      return converted.replace(/[0-9]/g, "dapi")
-    };
+        // const exp = /\s*,\s*|\s+,|[ ]+/g;  // a, b --> a_b
+        const exp = /\s|,/g;   // a, b --> a__b
 
-    // Getting all the table rows
-    const rows = table[0].querySelectorAll('tbody tr');
+        const converted = str.toLocaleLowerCase().replace(exp, "_",);
 
-    // Converting to array of object 
-    const rowData = Array.from(rows, row => {
+        // replacing number digits w/ 'dapi'
+        return converted.replace(/[0-9]/g, "dapi")
+      };
 
-      const tds = row.querySelectorAll('td'); // row value element (raw)
+      // Getting all the table rows
+      const rows: NodeListOf<Element> = table[0].querySelectorAll('tbody tr');
 
-      const data = Array.from(tds, td => td.textContent) // text values
-      // return null if row is empty
-      if (!data.length) return;
-      // return as per required format
-      return {
-        "iata": data[0],
-        "icao": data[1],
-        "airport_name": customString(data[2]),
-        "location_served": customString(data[3]),
-      }
+      // Converting to array of object 
+      const rowData: (IAirportData | null)[] = Array.from(rows,
+        (row): IAirportData | null => {
+
+          const tds: NodeListOf<Element> = row.querySelectorAll('td'); // row value element (raw)
+
+          const data: (string | null)[] = Array.from(tds, td => td.textContent) // text values
+          // return null if row is empty
+          if (!data.length) return null;
+          // return as per required format
+          return {
+            "iata": data[0] || "",
+            "icao": data[1] || "",
+            "airport_name": customString(data[2]),
+            "location_served": customString(data[3]),
+          }
+        })
+
+      // filter out null values
+      const noNullRowData = rowData.filter((val): boolean => !!val) as IAirportData[]
+
+      return noNullRowData;
     })
 
-    // filter out null values
-    return rowData.filter(val => !!val)
-  })
-
-  await browser.close();
+  await page.close()
 
   // return formatted object data for given page
-  return data;
+  return tableData;
 };
 
 
@@ -108,15 +127,18 @@ const scrapeList = async (link: string): Promise<any[]> => {
 // All inside a async f'n
 (async (): Promise<void> => {
   // Extractiong alphabet wise pages from home page
-  const alphabets_links = await getAllLinks();
+  const alphabets_links: IPagesLink[] = await getAllLinks();
+
+  // new Browser Instance
+  const browser: puppeteer.Browser = await puppeteer.launch();
 
   // Concurrency model for all promises within loop
-  const allCombined = await Promise.all(
+  const allCombined: IAirportData[][] = await Promise.all(
 
     // Looping through all links(pages)
     alphabets_links.splice(2,) // ignore first 2 links
-      .map(async ({ id, link }) => {   // will combine all pages data 
-        const list = await scrapeList(link)
+      .map(async ({ id, link }): Promise<IAirportData[]> => {   // will combine all pages data 
+        const list: IAirportData[] = await scrapeList(browser, link)
 
         // (optional) creating json for every page data
         createFile(`output/byPages/data_${id}.json`, list);
@@ -128,10 +150,13 @@ const scrapeList = async (link: string): Promise<any[]> => {
   // current object is like [ [array of page 1],  [..], [..], .....] becouse of maping.
 
   // Flattening array i.e [ [{}], [{}], [{}],...] -> to -> [{}, {},.....]
-  const flattenedData = allCombined.reduce((acc, cur) => acc.concat(...cur), [])
+  const flattenedData: IAirportData[] = allCombined.reduce((acc, cur) => acc.concat(...cur), [])
 
   // Finally creating combined file
   createFile(`output/All.json`, flattenedData);
+
+  await browser.close();
+
 })();
 
 
